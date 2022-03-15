4b77798b3c929f8b71750202128f2b6de96dbd99
Vinícius Bressanini <vinimarcus41@gmail.com>
2022-03-15 15:17:12 -0300

    Aula 17 - Exceções Customizadas

--- src/main/java/academy/devdojo/springboot2/service/AnimesService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimesService.java
@@ -1,6 +1,7 @@
 package academy.devdojo.springboot2.service;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.exception.BadRequestException;
 import academy.devdojo.springboot2.mapper.AnimeMapper;
 import academy.devdojo.springboot2.repositories.AnimeRepository;
 import academy.devdojo.springboot2.requests.AnimePostRequestBody;
@@ -30,7 +31,7 @@ public class AnimesService {
 
     public Anime findByIdOrThrowBadRequestException(long id) {
         return  animeRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime Not Found"));
+                .orElseThrow(() -> new BadRequestException("Anime Not Found"));
     }
 
     public Anime save(AnimePostRequestBody animePostRequestBody) {
