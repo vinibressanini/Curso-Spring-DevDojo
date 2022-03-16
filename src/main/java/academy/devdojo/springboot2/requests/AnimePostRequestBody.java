4da34c8b42ada9b6cc045e719ba81b2e6dd322a3
Vinícius Bressanini <vinimarcus41@gmail.com>
2022-03-16 15:46:52 -0300

    Aula 20 - Validação de campos

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -2,7 +2,10 @@ package academy.devdojo.springboot2.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class AnimePostRequestBody {
-    private String name;
+   @NotEmpty(message = "This attribute can´t be empty")
+   private String name;
 }
