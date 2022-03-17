aeecad387e9e75e5bb5c992296162dd6551601b9
Vinícius Bressanini <vinimarcus41@gmail.com>
2022-03-17 20:37:33 -0300

    Aula 23 - Paginação

--- src/main/java/academy/devdojo/springboot2/service/AnimesService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimesService.java
@@ -7,6 +7,8 @@ import academy.devdojo.springboot2.repositories.AnimeRepository;
 import academy.devdojo.springboot2.requests.AnimePostRequestBody;
 import academy.devdojo.springboot2.requests.AnimePutRequestBody;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,9 +20,9 @@ public class AnimesService {
 
     private final AnimeRepository animeRepository;
 
-    public List<Anime> listAll()
+    public Page<Anime> listAll(Pageable pageable)
     {
-        return animeRepository.findAll();
+        return animeRepository.findAll(pageable);
     }
 
     public List<Anime> findByName(String name)
