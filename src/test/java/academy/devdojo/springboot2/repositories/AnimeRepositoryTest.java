54ae8e7ebc1ca3971947598148347591bf8bc0d2
Vinícius Bressanini <vinimarcus41@gmail.com>
2022-03-29 15:21:42 -0300

    Aula 30 - Spring Data JPA Test pt 01

---
+++ src/test/java/academy/devdojo/springboot2/repositories/AnimeRepositoryTest.java
@@ -0,0 +1,35 @@
+package academy.devdojo.springboot2.repositories;
+
+import academy.devdojo.springboot2.domain.Anime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@DisplayName("Tests for Anime Repository")
+class AnimeRepositoryTest {
+
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    @Test
+    @DisplayName("Save Creates Anime When Sucessful")
+    void save_PersistAnime_WhenSucessful() {
+        Anime animeToBeSaved = createAnime();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+        Assertions.assertThat(animeSaved).isNotNull();
+        Assertions.assertThat(animeSaved.getId()).isNotNull();
+        Assertions.assertThat(animeSaved.getName()).isEqualTo(animeToBeSaved.getName());
+    }
+
+    private Anime createAnime () {
+        return Anime.builder()
+                .name("Kimetsu no Yaiba")
+                .build();
+    }
+
+}
